70388ca9711c43ebf16671ac18665296aa4113b5
Georgkali <georgijs.kalicava@gmail.com>
2021-12-22 09:46:35 +0200

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+<form method="post" action="/register">
+    @csrf
+
+    <input name="name">
+    <br>
+    <input name="email">
+    <br>
+
+    <input name="password">
+    <br>
+
+    <input name="password_confirmation">
+    <br>
+
+    <button>submit</button>
+
+</form>
+
+
+</body>
+</html>
